b5f169c105749895c52edfc1e617914d5674c97e
Tancredi Carluccio <tanc_@hotmail.it>
2022-03-24 00:44:43 +0100

    Prima prova per il controllo

--- main.js
+++ main.js
@@ -1,10 +1,6 @@
 // Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi.
 // Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
 // Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
-// Consigli del giorno:
-// * Pensate prima in italiano.
-// * Dividete in piccoli problemi la consegna.
-// * Individuate gli elementi di cui avete bisogno per realizzare il programma.
 
 let numeriCasuali = [];
 
@@ -20,6 +16,8 @@ setTimeout(trentaTimer, 3000);
 
 let numeriInseriti = [];
 
+let numerigiusti = 0;
+
 function trentaTimer() {
 
     for (let x = 0; x < 5; x++) {
@@ -27,7 +25,12 @@ function trentaTimer() {
         numeriInseriti.push(prompt("Inserisci uno dei 5 numeri visti in precedenza"));
         console.log(numeriInseriti);
 
-    }    
-    
-}
+        if (numeriInseriti[x].includes(numeriCasuali)) {
 
+            numerigiusti = + 1;
+            console.log(numeriInseriti[x])
+            console.log(numerigiusti)  
+        }
+
+    }
+}
